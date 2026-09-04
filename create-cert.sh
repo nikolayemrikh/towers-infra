868c216735b0e352e7798fdb72781cf24ab9bf9c
Nikolay Emrikh <redishko@gmail.com>
2026-09-04 21:53:44 +0300

    uncomment

--- create-cert.sh
+++ create-cert.sh
@@ -4,6 +4,6 @@ docker run --rm \
   -v $(pwd)/certbot/conf:/etc/letsencrypt \
   certbot/certbot certonly --webroot \
   --webroot-path=/var/www/certbot \
-  -d calls.magic-towers.xyz \
+  -d peer.mag-towers.xyz \
   --email redishko@gmail.com \
   --agree-tos --no-eff-email
